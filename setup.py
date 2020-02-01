cffeb25837d14affc22a7c1b0e7a53bce0185132
Hsuan-Hau Liu <hsuanhal@usc.edu>
2020-01-31 16:59:42 -0800

    update requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(
     author='Hsuan-Hau Liu',
     description='Simple security camera system right on your computer.',
     packages=['security_webcam',],
-    install_requires=['opencv-python', 'numpy'],
+    install_requires=[
+        'opencv-python>=4.1.1.26',
+        'numpy>=1.17.4'
+    ],
     entry_points={
         'console_scripts': [
             'security_webcam=security_webcam.__main__:main'
